fb19d17a29b00e604d95563a124aebef0de197cd
Junlong Wang <junlong.wang@serko.com>
2024-05-02 11:14:28 +1200

    Initialise Application layer

---
+++ Backend/TodoList.Application/OutboundPorts/ITodoRepository.cs
@@ -0,0 +1,11 @@
+﻿using TodoList.Domain;
+
+namespace TodoList.Application.OutboundPorts;
+
+public interface ITodoRepository
+{
+    Task<TodoItem?> GetAsync(Guid id);
+    Task<IEnumerable<TodoItem>> GetAllAsync();
+    Task<TodoItem> UpdateAsync();
+    Task<TodoItem> AddAsync();
+}
